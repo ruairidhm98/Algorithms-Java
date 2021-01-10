449510f31cf4dc38dee881d65c31b0c1d09fbe6e
ruairidhm98 <2250079m@student.gla.ac.uk>
2021-01-10 11:55:29 +0000

    Basic sumeuler implementation done

--- sumeuler/SumEuler.java
+++ sumeuler/SumEuler.java
@@ -1,19 +1,11 @@
 package sumeuler;
 
-import java.util.ArrayList;
-
 class SumEuler
 {
-  private ArrayList<Worker> workers;
-  private int nThreads;
+  SumEuler()
+  {}
 
-  SumEuler(int n)
-  {
-    workers = new ArrayList<>();
-    nThreads = n;
-  }
-
-  private long hcf(long x, long y)
+  private static long hcf(long x, long y)
   {
     if (y == 0)
     {
@@ -22,16 +14,16 @@ class SumEuler
     return hcf(y, x % y);
   }
 
-  private boolean relprime(long x, long y)
+  private static boolean relprime(long x, long y)
   {
     return hcf(x, y) == 1;
   }
 
-  private long euler(long n)
+  private static long euler(long n)
   {
     long length = 0;
 
-    for(long i = 0; i < n; ++i)
+    for(long i = 1; i < n; ++i)
     {
       if (relprime(n, i))
       {
@@ -41,7 +33,7 @@ class SumEuler
     return length;
   }
 
-  public long sumTotient(long lower, long upper)
+  public static long sumTotient(long lower, long upper)
   {
     long sum = 0;
     
@@ -52,33 +44,21 @@ class SumEuler
     return sum;
   }
 
-  public long parallelise(long lower, long upper)
-  {
-    
-    for (int i = 0; i < nThreads; ++i)
-    {
-      //workers.add(new Worker(this, i));
-    }
-
-    return 0L;
-  }
-
   public static void main(String args[])
   {
-    if (args.length < 4)
+    if (args.length != 2)
     {
-      System.err.println("Error: not 3 arguments. Please provide lower, upper and number of threads");
+      System.err.println("Error: not 2 arguments. Please provide lower, upper");
       return;
     }
 
     long lower = Long.parseLong(args[0]), upper = Long.parseLong(args[1]);
-    int nThreads = Integer.parseInt(args[2]);
 
-    SumEuler task = new SumEuler(nThreads);
-    task.sumTotient(lower, upper);
-    
-
-
+    long start = System.currentTimeMillis();
+    long result = SumEuler.sumTotient(lower, upper);
+    long end = System.currentTimeMillis();
+    System.out.println("Sum of Totient: " + result);
+    System.out.println("Elapsed time: " + (end-start) + "ms");
   }
 
 }
